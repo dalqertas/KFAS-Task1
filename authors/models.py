47058d48869a46c527fd4ac909b764f3618f6898
dalqertas <dunyaalqertas@gmail.com>
2020-01-26 19:27:24 +0300

    done

---
+++ authors/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Authors(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    imageURL = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+class Books(models.Model):
+    RED= 'RED'
+    BLUE = 'BLUE'
+    GREEN = 'GREEN'
+    colors = [
+        (RED, ('RED')),
+        (BLUE, ('BLUE')),
+        (GREEN, ('GREEN')),
+    ]
+    title = models.CharField(max_length=120)
+    available = models.BooleanField()
+    color=  models.CharField(max_length=120, choices=colors, default=RED)
+    author= models.ForeignKey(Authors, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
